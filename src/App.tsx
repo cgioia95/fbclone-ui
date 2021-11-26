425eecabc6f070efa8b13633173f5957bd1ad33f
Christian Gioia <ChristianJamesAttilio.Gioia@team.telstra.com>
2021-11-26 14:55:46 +1100

    setup boilerplate code for typescript react, material ui baseline/themeprovider and basic login page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoginPage from './Login/Containers/LoginPage'
+
+
+function App() {
+  return (
+    <div className="App">
+        <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login/Containers/LoginPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+LoginPage.propTypes = {
+    
+};
+
+function LoginPage() {
+    return (
+        <div>
+            <h1> Login Page</h1>
+            
+        </div>
+    );
+}
+
+export default LoginPage;
